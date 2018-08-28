4b8528991e6ff6bc31813ba5e9f15599ae70f097
Casaucau Cyril <raiden0610@gmail.com>
2018-08-28 18:49:03 +0200

    install mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
-sed 's,{{DB_HOST}},'"${DB_HOST}"',g' -i /var/www/html/_config/config.php
-sed 's,{{DB_DATABASE}},'"${DB_DATABASE}"',g' -i /var/www/html/_config/config.php
-sed 's,{{DB_USERNAME}},'"${DB_USERNAME}"',g' -i /var/www/html/_config/config.php
-sed 's,{{DB_PASSWORD}},'"${DB_PASSWORD}"',g' -i /var/www/html/_config/config.php
-sed 's,{{DB_PREFIX}},'"${DB_PREFIX}"',g' -i /var/www/html/_config/config.php
-sed 's,{{LIVEZILLA_ID}},'"${LIVEZILLA_ID}"',g' -i /var/www/html/_config/config.php
-sed 's,{{LIVEZILLA_PR_CR}},'"${LIVEZILLA_PR_CR}"',g' -i /var/www/html/_config/config.php
+
+if [ "${INSTALL_MODE}" = "false" ]; then
+	sed 's,{{DB_HOST}},'"${DB_HOST}"',g' -i /var/www/html/_config/config.php
+	sed 's,{{DB_DATABASE}},'"${DB_DATABASE}"',g' -i /var/www/html/_config/config.php
+	sed 's,{{DB_USERNAME}},'"${DB_USERNAME}"',g' -i /var/www/html/_config/config.php
+	sed 's,{{DB_PASSWORD}},'"${DB_PASSWORD}"',g' -i /var/www/html/_config/config.php
+	sed 's,{{DB_PREFIX}},'"${DB_PREFIX}"',g' -i /var/www/html/_config/config.php
+	sed 's,{{LIVEZILLA_ID}},'"${LIVEZILLA_ID}"',g' -i /var/www/html/_config/config.php
+	sed 's,{{LIVEZILLA_PR_CR}},'"${LIVEZILLA_PR_CR}"',g' -i /var/www/html/_config/config.php
+	
+	rm -rf /var/www/html/install
+else
+	rm -rf /var/www/html/_config/config.php
+fi
+
 
 start_system() {
   /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
